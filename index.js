13347e982c56746fb6aaadc81b034d8041e17523
DronVats <87650346+DronVats@users.noreply.github.com>
2024-03-31 13:44:14 +0530

    firstt

---
+++ index.js
@@ -0,0 +1,71 @@
+// ---
+const hamMenuBtn = document.querySelector('.header__main-ham-menu-cont')
+const smallMenu = document.querySelector('.header__sm-menu')
+const headerHamMenuBtn = document.querySelector('.header__main-ham-menu')
+const headerHamMenuCloseBtn = document.querySelector(
+  '.header__main-ham-menu-close'
+)
+const headerSmallMenuLinks = document.querySelectorAll('.header__sm-menu-link')
+
+hamMenuBtn.addEventListener('click', () => {
+  if (smallMenu.classList.contains('header__sm-menu--active')) {
+    smallMenu.classList.remove('header__sm-menu--active')
+  } else {
+    smallMenu.classList.add('header__sm-menu--active')
+  }
+  if (headerHamMenuBtn.classList.contains('d-none')) {
+    headerHamMenuBtn.classList.remove('d-none')
+    headerHamMenuCloseBtn.classList.add('d-none')
+  } else {
+    headerHamMenuBtn.classList.add('d-none')
+    headerHamMenuCloseBtn.classList.remove('d-none')
+  }
+})
+
+for (let i = 0; i < headerSmallMenuLinks.length; i++) {
+  headerSmallMenuLinks[i].addEventListener('click', () => {
+    smallMenu.classList.remove('header__sm-menu--active')
+    headerHamMenuBtn.classList.remove('d-none')
+    headerHamMenuCloseBtn.classList.add('d-none')
+  })
+}
+
+// ---
+const headerLogoConatiner = document.querySelector('.header__logo-container')
+
+headerLogoConatiner.addEventListener('click', () => {
+  location.href = 'index.html'
+})
+
+
+
+
+
+$(document).ready(function() {
+  var n = '#nav', no = 'nav-open';
+	$('#nav-menu').click(function(){
+		if ($(n).hasClass(no)){
+			$(n).animate({height:0},300);
+			setTimeout(function(){
+				$(n).removeClass(no).renoveAttr('style')
+			},320);
+		}
+		else{
+			
+			var newH = $(n).css('height', 'auto').height();
+			$(n).height(0).animate({height:newH}, 300);
+			setTimeout(function(){
+				$(n).addClass(no).renoveAttr('style')
+			},320);
+		}
+	});
+});
+
+
+
+$(function() {
+  $('.scroll-down').click (function() {
+    $('html, body').animate({scrollTop: $('section.ok').offset().top }, 'slow');
+    return false;
+  });
+});
